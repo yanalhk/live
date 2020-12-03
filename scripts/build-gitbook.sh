863ce751941f93f5b02fdbe426dc8db137fed049
Ben Lau <xbenlau@gmail.com>
2020-12-03 16:36:29 +0800

    Fix broken travis script

--- scripts/build-gitbook.sh
+++ scripts/build-gitbook.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-realpath() {
-  #Works on both of Mac and Linux
-  node -e "const {resolve} = require('path'); console.log(resolve('$1'))";
-}
-
 pushd () {
     command pushd "$@" > /dev/null
 }
@@ -15,10 +10,9 @@ popd () {
 
 set -e
 
-SCRIPT_DIR=$(realpath $(dirname "$0"))
-BASE_DIR=`realpath $SCRIPT_DIR/..`
-GITBOOK_DIR=`realpath ${BASE_DIR}/gitbook`
-GITBOOK_HTML_DIR=`realpath ${GITBOOK_DIR}/_book`
+BASE_DIR="$PWD"
+GITBOOK_DIR="$BASE_DIR/gitbook"
+GITBOOK_HTML_DIR="${GITBOOK_DIR}/_book"
 
 pushd $GITBOOK_DIR
 if [ ! -d 'node_modules' ]
@@ -26,6 +20,7 @@ then
   docker run -ti --rm -v `pwd`:/docs humangeo/gitbook install
 fi
 docker run -ti --rm -v `pwd`:/docs humangeo/gitbook build
+
 sed -i.bk 's/yanalhk.github.io/yanalhk.github.io\/live/g' $GITBOOK_HTML_DIR/sitemap.xml
 rm $GITBOOK_HTML_DIR/sitemap.xml.bk
 popd
