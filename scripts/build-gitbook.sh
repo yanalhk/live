2ed787bdab8ba09660c4da836e6bf7289a0f8ffd
Ben Lau <xbenlau@gmail.com>
2020-12-03 15:10:03 +0800

    Fix broken sitemap.xml

--- scripts/build-gitbook.sh
+++ scripts/build-gitbook.sh
@@ -5,6 +5,14 @@ realpath() {
   node -e "const {resolve} = require('path'); console.log(resolve('$1'))";
 }
 
+pushd () {
+    command pushd "$@" > /dev/null
+}
+
+popd () {
+    command popd "$@" > /dev/null
+}
+
 set -e
 
 SCRIPT_DIR=$(realpath $(dirname "$0"))
@@ -18,4 +26,6 @@ then
   docker run -ti --rm -v `pwd`:/docs humangeo/gitbook install
 fi
 docker run -ti --rm -v `pwd`:/docs humangeo/gitbook build
+sed -i.bk 's/yanalhk.github.io/yanalhk.github.io\/live/g' $GITBOOK_HTML_DIR/sitemap.xml
+rm $GITBOOK_HTML_DIR/sitemap.xml.bk
 popd
